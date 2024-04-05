af4f0600c51bef12bab8e63988e4389ffab37bc4
mikemilk <3297233599@qq.com>
2024-04-05 16:11:42 +0800

    test

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-// 确保调用marked.parse之前已引入marked库
-// fetch('content/test.md')
-//     .then(response => response.text())
-//     .then(text => {
-//         const htmlContent = marked.parse(text);
-//         document.getElementById('blog').innerHTML = htmlContent;
-//     })
-//     .catch(error => console.error(error));
-const links = document.querySelectorAll('.nav-link');
-const contents = document.querySelectorAll('.content');
-
-links.forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('data-target');
-        contents.forEach(content => {
-            if(content.id === targetId) {
-                content.classList.add('active');
-            } else {
-                content.classList.remove('active');
-            }
-        });
-    });
-});
-
-
-
-
+// 当页面加载完毕时
+window.onload = function() {
+    // 直接设置URL的hash来跳转到Home部分
+    window.location.hash = "Home";
+};
